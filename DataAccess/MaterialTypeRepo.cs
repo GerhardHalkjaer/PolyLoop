bc0dc5e47a670047cf11088de9cba5df54df7693
g.halkjaer@gmail.com <Gigancc1701E>
2025-02-20 13:40:44 +0100

    setup 2 blazor project for production and management and api

---
+++ DataAccess/MaterialTypeRepo.cs
@@ -0,0 +1,44 @@
+﻿using Entities;
+using Microsoft.Data.SqlClient;
+
+
+namespace DataAccess
+{
+    public class MaterialTypeRepo
+    {
+        private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PolyLoop;
+                                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                    Trust Server Certificate=False;Application Intent=ReadWrite;
+                                    Multi Subnet Failover=False";
+
+
+        public List<MaterialType> GetAll()
+        {
+            List<MaterialType> materialTypes = new List<MaterialType>();
+
+            string query = "SELECT * FROM MaterialTypes";
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            SqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                // TODO fill
+            }
+
+            con.Close();
+
+            return null;
+        }
+
+
+        public void SaveNew(MaterialType material)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ PolyLoopApi/Controllers/MaterialTypeController.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PolyLoopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialTypeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<MaterialType>> GetAll()
+        {
+            MaterialTypeRepo materialType = new MaterialTypeRepo();
+            List<MaterialType> result = materialType.GetAll();
+            return result;
+        }
+    }
+}
